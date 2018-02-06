e447d83c2ed0e80a48cbbd1998d658531fc2a5a9
黒田建彰 <tatsuaki@orange.ocn.ne.jp>
2018-02-06 22:56:09 +0900

    タイムラインの直前までコピペ終了。早く終わらしてしまって、ゆっくりテキストを読み返そう。

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by kuroda tatsuaki on 2018/02/06.
+//  Copyright © 2018年 tatsuaki.kuroda. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
