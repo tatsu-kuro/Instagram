e60c86f12d563ca84c46796dfa4dff3a0c9ea1e6
黒田建彰 <tatsuaki@orange.ocn.ne.jp>
2018-02-09 13:36:15 +0900

    captionLabel_topを作った。ここに投稿者のコメントを書き、captionLabelには閲覧者のコメントを書く

--- Instagram/PostTableViewCell.swift
+++ Instagram/PostTableViewCell.swift
@@ -19,6 +19,7 @@ class PostTableViewCell: UITableViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var captionLabel: UILabel!
     
+    @IBOutlet weak var captionLabel_top: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
